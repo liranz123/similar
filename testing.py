b9ebe111673d64c5e6e00ac7b4917766e3be00c2
liranz123 <liranziv1@gmail.com>
2021-07-06 08:24:36 +0300

    commit

--- testing.py
+++ testing.py
@@ -2,17 +2,22 @@ import json, datetime, time, dateutil
 from dateutil import parser
 from datetime import datetime, timedelta
 
-starts = "May 23 21:01:10 2021 GMT"
-ends = "May 24 20:01:10 2021 GMT"
+def func(file, domain, allowed):
 
-starts = starts.replace('GMT', '%') # get rid of the gmt string 
-ends = ends.replace('GMT', '%')
+    f = open(file)
+    data = json.load(f)
+    starts = data['similarweb.com']['notBefore']
+    ends = data['similarweb.com']['notAfter']
 
-starts = time.mktime(datetime.strptime(starts, "%b %d %H:%M:%S %Y %%").timetuple())
-ends = time.mktime(datetime.strptime(ends, "%b %d %H:%M:%S %Y %%").timetuple())
+    starts = starts.replace('GMT', '%') # get rid of the gmt string 
+    ends = ends.replace('GMT', '%')
 
-diff = ends - starts
+    starts = time.mktime(datetime.strptime(starts, "%b %d %H:%M:%S %Y %%").timetuple())
+    ends = time.mktime(datetime.strptime(ends, "%b %d %H:%M:%S %Y %%").timetuple())
 
-print(diff/3600)
+    diff = ends - starts
 
+    print(diff/3600/24) 
 
+if __name__ == '__main__':
+    func('domains.json', 'similarweb.com', 30)
